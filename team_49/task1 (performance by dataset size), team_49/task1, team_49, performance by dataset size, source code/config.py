3b703564e0719c0bb243729a6b385bbc5c4eff20
ShrubinS <shrubin93.skn@gmail.com>
2017-10-23 15:35:56 +0100

    first-commit

---
+++ team_49/task1 (performance by dataset size), team_49/task1, team_49, performance by dataset size, source code/config.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+chunks = [100, 500, 1000, 5000, 10000, 50000, 100000, 500000, 1000000, 5000000, 10000000, 50000000, 100000000];
+
+DATASET_ROOT = '/Users/apple/projects/CS7CS4-machine-learning/Machine Learning Datasets'
+
+DATA_SETS = {
+	'sum_without_noise' : DATASET_ROOT + '/the sum dataset/without noise/The SUM dataset, without noise.csv',
+	'sum_with_noise' : DATASET_ROOT + '/the sum dataset/with noise/The SUM dataset, with noise.csv'
+}
---
+++ team_49/task1 (performance by dataset size), team_49/task1, team_49, performance by dataset size, source code/regression_algorithms.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+# Imports
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn import linear_model
+from sklearn.metrics import mean_squared_error
+import math
+import matplotlib.pyplot as plt
+from sklearn.model_selection import cross_val_score
+
