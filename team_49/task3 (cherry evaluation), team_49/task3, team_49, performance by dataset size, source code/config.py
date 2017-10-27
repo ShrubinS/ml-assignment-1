fe6f11918f80db9f85143aa5e0e81026e20275df
Saksham Sinha <saksham290992@gmail.com>
2017-10-27 17:27:34 +0100

    Final commit config

--- team_49/task3 (cherry evaluation), team_49/task3, team_49, performance by dataset size, source code/config.py
+++ team_49/task3 (cherry evaluation), team_49/task3, team_49, performance by dataset size, source code/config.py
@@ -1,13 +1,8 @@
 DATASET_ROOT = '/home/saksham/ML/Datasets/'
 
 DATA_SETS = {
-	# 'sum_without_noise' : DATASET_ROOT + '/the sum dataset/without noise/The SUM dataset, without noise.csv',
-	# 'sum_with_noise' : DATASET_ROOT + '/The SUM dataset, without noise.csv',
-	#'kc_house_data'	: DATASET_ROOT + '/kc_house_data.csv',
    'skinnonskin'	: DATASET_ROOT + '/Skin_NonSkin.txt',
-    # 'susy'          : DATASET_ROOT + '/SUSY.csv/SUSY.csv',
-    'sum'       : DATASET_ROOT + '/The SUM dataset, without noise.csv',
-    # 'titanic'	: DATASET_ROOT + '/titanic dataset.csv'
+    'sum'       : DATASET_ROOT + '/The SUM dataset, without noise.csv'
 }
 
 outputfile='/home/saksham/ML/output.txt'
